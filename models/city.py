11551711ed580bb4b7df6ccfec6faefa6f1a3619
Adeyemi Samuel Adewale <Adeyemifrodo@gmail.com>
2023-10-04 23:26:44 +0000

    City class created

---
+++ models/city.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""A module that defines city of user"""
+
+from models.base_model import BaseModel
+
+class City(BaseModel):
+	"""A city class definition"""
+	state_id = ""
+	name = ""
