488f403d0b9d4343615821398f50b7c24b05d9c2
Adeyemi Samuel Adewale <Adeyemifrodo@gmail.com>
2023-10-04 02:10:22 +0000

    Test

--- tests/test_console.py
+++ tests/test_console.py
@@ -10,11 +10,14 @@ import unittest
 class Test_console(unittest.TestCase):
     def test_help(self):
         with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().onecmd("help") #execute the help command
-            output = f.getvalue() #get the captured value as a string
-            expect = '''Documented commands (type help <topic>):
-========================================
-EOF  help  quit\n\n'''
+            HBNBCommand().onecmd("help") # execute the help command
+            output = f.getvalue() # get the captured value as a string
+            expect = 'Documented commands (type help <topic>):\n========================================\nEOF  help  quit\n'
+
+        # Remove any trailing whitespace, including newlines, for comparison
+        output = output.strip()
+        expect = expect.strip()
+
         self.assertEqual(output, expect)
 
 if __name__ == '__main__':
