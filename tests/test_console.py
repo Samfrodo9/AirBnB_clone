e5e92e1891c0f5aef5aa2463637371b588fd149b
Onyenze Chetachi <chetaGod4life@gmail.com>
2023-10-09 23:33:13 +0100

    op

--- tests/test_console.py
+++ tests/test_console.py
@@ -4,10 +4,18 @@
 from unittest.mock import patch
 from io import StringIO
 import sys
-from AirBnB_clone.console import HBNBCommand
+from console import HBNBCommand
+import unittest
 
-with patch('sys.stdout', new=StringIO()) as f:
+class Test_console(unittest.TestCase):
+    def test_help(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("help") #execute the help command
+            output = f.getvalue() #get the captured value as a string
+            expect = '''Documented commands (type help <topic>):
+========================================
+EOF  help  quit\n\n'''
+        self.assertEqual(output, expect)
 
-    HBNBCommand().onecmd("help") #execute the help command
-    output = f.getvalue() #get the captured value as a string
-assert output == "Documented commands (type help <topic>):\n========================================\nEOF  help  quit\n"
+if __name__ == '__main__':
+    unittest.main()
