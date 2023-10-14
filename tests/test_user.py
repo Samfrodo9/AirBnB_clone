87edc2c2e6f9b7fb9f4895b838130c17115431c2
Onyenze Chetachi <chetaGod4life@gmail.com>
2023-10-14 21:20:12 +0100

    an

---
+++ tests/test_user.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''test cases for user'''
+
+import models
+from console import HBNBCommand
+import unittest
+
+class Test_user(unittest.Testcase):
+    '''test units'''
+    def set_up():
+        '''set up refactoring'''
+        rm storage.py
+
+    def 
